765ca926fd76196f584d2f6373d5660282acd794
Ernesto <44853461+ernestoherrerab@users.noreply.github.com>
2022-03-29 10:37:29 +0200

    Initial Commit

--- network_automation/__init__.py
+++ network_automation/__init__.py
@@ -7,6 +7,7 @@ sys.dont_write_bytecode = True
 from decouple import config
 from flask import Flask, render_template, redirect
 from network_automation.ise_mac_bypass.views import ise_mac_bypass
+from network_automation.nw_infra_links.views import nw_infra_links
 
 FLASK_KEY = config("FLASK_SECRET_KEY")
 
@@ -16,6 +17,7 @@ app.secret_key = FLASK_KEY
 
 ### REGISTER BLUEPRINTS OF SECONDARY APPS ###
 app.register_blueprint(ise_mac_bypass, url_prefix="/ise-mac-bypass")
+app.register_blueprint(nw_infra_links, url_prefix="/nw-infra-links")
 
 ### VIEWS OR ROUTES ###
 @app.route("/home")
