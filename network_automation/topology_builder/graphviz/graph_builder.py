cafe09c2d65eb7556aa8a6876180fbcf2b01472e
Ernesto <44853461+ernestoherrerab@users.noreply.github.com>
2022-04-12 09:33:49 +0200

    Icons per Dev Type

--- network_automation/topology_builder/graphviz/graph_builder.py
+++ network_automation/topology_builder/graphviz/graph_builder.py
@@ -4,7 +4,6 @@ Module to add visual description to switches
 """
 
 from graphviz import Digraph
-from pathlib import Path
 import re
 
 def host_list(hosts):
@@ -18,29 +17,37 @@ def gen_graph(name, source_list, filename):
     """
     Generate Graph
     """
-    
+
     dot = Digraph(name, format="png")
-    dot.attr("node", shape="box")
-    #dot.attr("node", image="./images/l3_sw.png")
     dot.attr("edge", arrowhead="none")
     dot.format = "png"
     dot.graph_attr["splines"] = "ortho"
-    
+
     ### GENERATE EDGE RELATIONSHIPS ###
     for edges in source_list:
         for node in edges:
             host_type = re.findall(r"^\w+-([a-z]+|[A-Z]+)", node)
             if host_type and host_type[0] == "cs":
-                dot.attr("node", image="./images/l3_sw.png")
+                dot.node(node, image="./images/l3_sw.png", shape="box")
             elif host_type and host_type[0] == "as":
-                dot.attr("node", image="./images/access_sw.png")
+                dot.node(node, image="./images/access_sw.png", shape="box")
             elif host_type and host_type[0] == "wlc":
-                dot.attr("node", image="./images/wlc.png")    
+                dot.node(node, image="./images/wlc.png", shape="box")
             elif host_type and host_type[0] == "ds":
-                dot.attr("node", image="./images/dist_sw.png")   
+                dot.node(node, image="./images/dist_sw.png", shape="box")
             elif host_type and host_type[0] == "ap":
-                dot.attr("node", image="./images/accesspoint.png")   
+                dot.node(node, image="./images/accesspoint.png", shape="box")
+            elif host_type and host_type[0] == "uc":
+                dot.node(node, image="./images/unityserver.png", shape="box")
+            elif host_type and host_type[0] == "edgertr":
+                dot.node(node, image="./images/router.png", shape="box")
+            elif host_type and host_type[0] == "ron":
+                dot.node(node, image="./images/router.png", shape="box")
+            elif host_type and host_type[0] == "rcrtr":
+                dot.node(node, image="./images/router.png", shape="box")
+            elif not host_type and node.startswith("SEP"):
+                dot.node(node, image="./images/phone.png", shape="box")
             else:
-                dot.attr("node", image="./images/nx_sw.png")    
+                dot.node(node, shape="box")
         dot.edge(edges[0],edges[1])
     dot.render(filename=filename, format="png")
