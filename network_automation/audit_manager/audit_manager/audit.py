08fa1295fd7fb086ef00772f73a7665a8c24b0d3
Ernesto <44853461+ernestoherrerab@users.noreply.github.com>
2022-08-05 10:49:46 +0200

    Added STP with MST

--- network_automation/audit_manager/audit_manager/audit.py
+++ network_automation/audit_manager/audit_manager/audit.py
@@ -12,6 +12,7 @@ import network_automation.audit_manager.audit_manager.parsers.getAAA as getAAA
 import network_automation.audit_manager.audit_manager.parsers.getBase as getBase
 import network_automation.audit_manager.audit_manager.parsers.getInterfaces as getInterfaces
 import network_automation.audit_manager.audit_manager.parsers.getNTP as getNTP
+import network_automation.audit_manager.audit_manager.parsers.getSTP as getSTP
 import network_automation.audit_manager.audit_manager.parsers.getSVI as getSVI
 import network_automation.audit_manager.audit_manager.parsers.getVLAN as getVLAN
 
@@ -70,3 +71,8 @@ def do_audit(username, password, depth_levels=3):
         print("Getting interfaces Configs...")
         dev_data[dev_config[0]] = getInterfaces.audit_interfaces(parse_obj)
         build_yml_file("interfaces", dev_data[dev_config[0]], dev_audit_path)
+
+        #### PARSE STP & DUMP INTO FILE ###
+        print("Getting STP Configs...")
+        dev_data[dev_config[0]] = getSTP.audit_stp(parse_obj)
+        build_yml_file("stp", dev_data[dev_config[0]], dev_audit_path)
