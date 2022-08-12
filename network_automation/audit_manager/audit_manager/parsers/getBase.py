e52d73fdfb56ed248cb06ae2ff57dc121e1a8403
Ernesto <44853461+ernestoherrerab@users.noreply.github.com>
2022-08-12 16:54:54 +0200

    Added logging to base

--- network_automation/audit_manager/audit_manager/parsers/getBase.py
+++ network_automation/audit_manager/audit_manager/parsers/getBase.py
@@ -2,6 +2,7 @@
 """
 Script to parse Base configs
 """
+from netaddr import valid_ipv4
 
 def audit_base(parse_obj):
     
@@ -10,11 +11,13 @@ def audit_base(parse_obj):
     dev_data["base"] = {}
     dev_data["base"]["timestamps"] = []
     dev_data["base"]["domain"] = {}
+    dev_data["base"]["logging"] = {}
     base_service_lines = parse_obj.find_objects(r"^service")
     base_domainlookup_line = parse_obj.find_objects(r"^ip domain-lookup")
     base_domainname_line = parse_obj.find_objects(r"^ip domain-name")
     base_httpserver_line = parse_obj.find_objects(r"^ip http server")
     base_httpsserver_line = parse_obj.find_objects(r"^ip http secure-server")
+    base_logging_line = parse_obj.find_objects(r"^logging")
 
     for base_service_line in base_service_lines:
         if "timestamps" in base_service_line.text:
@@ -42,5 +45,17 @@ def audit_base(parse_obj):
     else:
         dev_data["base"]["https_server"] = False
 
+    for base_logging in base_logging_line:
+        base_logging_text = base_logging.text
+        if "trap" in base_logging_text:
+            dev_data["base"]["logging"]["trap"] = base_logging_text.replace("logging trap " , "")
+        elif "source-interface" in base_logging_text:
+            dev_data["base"]["logging"]["source_interface"] = base_logging_text.replace("logging source-interface " , "")
+        elif valid_ipv4(base_logging_text.replace("logging ", "")) and "hosts" not in dev_data["base"]["logging"]:
+            dev_data["base"]["logging"]["hosts"] = {}
+            dev_data["base"]["logging"]["hosts"][base_logging_text.replace("logging ", "")] = {}
+        elif valid_ipv4(base_logging_text.replace("logging ", "")) and "hosts" in dev_data["base"]["logging"]:
+            dev_data["base"]["logging"]["hosts"][base_logging_text.replace("logging ", "")] = {}
+        
     return dev_data
 
