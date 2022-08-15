18603a9a39d7207bc40bc916e819e03eb8105be1
Ernesto <44853461+ernestoherrerab@users.noreply.github.com>
2022-08-15 16:14:25 +0200

    Update getSNMP.py

--- network_automation/audit_manager/audit_manager/parsers/getSNMP.py
+++ network_automation/audit_manager/audit_manager/parsers/getSNMP.py
@@ -18,6 +18,7 @@ def audit_snmp(parse_obj):
         snmp_line_text = snmp_line.text
         snmp_communities = re.findall(r'snmp-server\scommunity\s(\S+)\s(\w+)\s(\S+)', snmp_line_text)
         snmp_location = re.findall(r'snmp-server\slocation\s(\S+)', snmp_line_text)
+        snmp_contact = re.findall(r'snmp-server\scontact\s(\S+)', snmp_line_text)
         snmp_traps = re.findall(r'snmp-server\senable\straps\s(.+)$', snmp_line_text)
         snmp_host = re.findall(r'snmp-server\shost\s(\S+)\s(\w+)', snmp_line_text)
         if snmp_communities and "communities" not in dev_data["snmp_server"]:
@@ -33,6 +34,8 @@ def audit_snmp(parse_obj):
                 dev_data["snmp_server"]["communities"][snmp_communities[0][0]]["access_list_ipv4"] = snmp_communities[0][2]
         elif snmp_location: 
             dev_data["snmp_server"]["location"] = snmp_location[0]
+        elif snmp_contact: 
+            dev_data["snmp_server"]["contact"] = snmp_contact[0]
         elif snmp_traps and "snmp_traps" not in dev_data["snmp_server"]["traps"]:
             dev_data["snmp_server"]["traps"]["snmp_traps"] = []
             dev_data["snmp_server"]["traps"]["snmp_traps"].append(snmp_traps[0])
