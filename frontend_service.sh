f7e49bd091567b72346fb9c9896481ce1492c3cc
Ernesto <44853461+ernestoherrerab@users.noreply.github.com>
2022-08-19 14:58:36 +0200

    Update frontend_service.sh

--- frontend_service.sh
+++ frontend_service.sh
@@ -11,3 +11,13 @@ source bin/activate
 cd /home/ehb/automation/.venv/py_nw_automation
 python app.py -e prod &
 fi
+
+if (( $(ps -ef | grep -v grep | grep py_nw_automation/file_display/node_modules/react-scripts/scripts/start.js | wc -l) > 0 ))
+then
+echo "React APP is running!!!"
+else
+cd /home/ehb/automation/.venv
+source bin/activate
+cd /home/ehb/automation/.venv/py_nw_automation/file_display
+npm start &
+fi
