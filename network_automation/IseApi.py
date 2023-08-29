af07b55dba2cbc9c25b81c6de26139243b8a6a77
Ernesto <44853461+ernestoherrerab@users.noreply.github.com>
2023-08-29 19:28:33 +0200

    Guest Network Test

---
+++ network_automation/IseApi.py
@@ -0,0 +1,312 @@
+#! /usr/bin/env python
+"""
+Module to group the CRUD operations for API calls
+"""
+from json import loads, dumps
+import logging
+from pathlib import Path 
+from requests import Response, RequestException, Session
+import xml.etree.ElementTree as ET
+import xmltodict 
+from urllib.parse import urlencode
+
+### LOGGING SETUP ###
+LOG_FILE = Path("logs/ise_ops.log")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(LOG_FILE)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+class Ise(object):
+    
+    def __init__(self, basic_auth, accept="json"):
+        self.basic_auth = basic_auth
+        self.accept = accept
+        self.session = Session()
+        self.session.headers.update({'Authorization': f'Basic {self.basic_auth}', 'Content-Type': f'application/json','Accept': f'application/{self.accept}'})
+        self.session.verify = False
+
+    def get_operations(self, ops_type: str, url_var: str, params={}) -> Response:
+        """
+        Makes an API GET request to retrieve operations data.
+
+        Args:
+            ops_type (str): The type of operations to retrieve.
+            url_var (str): The base URL for the API endpoint.
+
+        Returns:
+            Response: A Response object containing the operations data.
+
+        Raises:
+            AuthenticationError: If the API request returns a 401 status code.
+            PermissionError: If the API request returns a 403 status code.
+            ServerError: If the API request returns a 500 status code.
+            RequestError: If the API request fails for an unknown reason.
+
+        """
+        ### VARS ###
+        self.ops_type = ops_type
+        self.url_var = url_var
+        self.params = params
+        url = f"{self.url_var}/{self.ops_type}"
+
+        ops_get = self.session.get(url, params=self.params, timeout=20.0)
+ 
+        if ops_get.status_code == 200 and self.accept=="json":
+            ops_data = loads(ops_get.text)
+            logger.info(f'API Call: {ops_get.status_code} {ops_data}')
+            ops_get.close()
+            return ops_data
+        
+        elif ops_get.status_code == 200 and self.accept=="xml":
+            ops_xml = ops_get.text
+            ops_json = xmltodict.parse(ops_xml)
+            ops_data = dumps(ops_json)
+            logger.info(f'API Call: {ops_get.status_code} {ops_data}')
+            ops_get.close()
+            return loads(ops_data)
+
+        elif ops_get.status_code == 401:
+            print("Authentication Issue. Login again.")
+            ops_get.close()
+            logger.info(f'API Call: {ops_get.status_code} Authentication Issue')
+            return ops_get.status_code
+
+        elif ops_get.status_code == 403:
+            print("Insufficient permissions to access this resource.")
+            ops_get.close()
+            logger.info(f'API Call: {ops_get.status_code} Insufficient permissions to access this resource.')
+            return ops_get.status_code
+
+        elif ops_get.status_code == 500:
+            print("Unexpected server side error.")
+            ops_get.close()
+            logger.info(f'API Call: {ops_get.status_code} Unexpected server side error.')
+            return ops_get.status_code
+
+        else:
+            print("GET Request Failed...Unknown why")
+            ops_get.close()
+            logger.info(f'API Call: {ops_get.status_code} Request Failed...Unknown why')
+            return ops_get.status_code
+
+    def post_operations(self, ops_type: str, url_var: str, payload: dict, params={}, json=True) -> Response:
+        """ API POST operations """
+
+        ### VARS ###
+        self.ops_type = ops_type
+        self.url_var = url_var
+        self.payload = payload
+        self.params = params
+        encoded_params = urlencode(self.params)
+        self.json = json
+        url = f"{self.url_var}/{self.ops_type}"
+        
+        if self.json:
+            try:
+                ops_post = self.session.post(url, params=encoded_params, json=self.payload)
+                if ops_post.status_code == 201:
+                    print("POST Request Successful!")
+                    ops_data = loads(ops_post.text)
+                    ops_post.close()
+                    logger.info(f'API Call: {ops_post.status_code} POST Request Successful! {ops_data}')
+                    return ops_data, ops_post.status_code    
+                elif ops_post.status_code == 400:
+                    print("JSON error. Check the JSON format.")
+                    ops_post.close()
+                    logger.error(f'API Call: {ops_post.status_code} JSON error. Check the JSON format.')
+                    return ops_post.status_code
+                elif ops_post.status_code == 401:
+                    print("Token error. Login again.")
+                    ops_post.close()
+                    logger.error(f'API Call: {ops_post.status_code} Authentication error.')
+                    return ops_post.status_code
+                elif ops_post.status_code == 403:
+                    print("Insufficient permissions to access this resource.")
+                    ops_post.close()
+                    logger.error(f'API Call: {ops_post.status_code} Insufficient permissions to access this resource.')
+                    return ops_post.status_code
+                elif ops_post.status_code == 409:
+                    print("The submitted resource conflicts with another.")
+                    ops_post.close()
+                    logger.error(f'API Call: {ops_post.status_code} The submitted resource conflicts with another.')
+                    return ops_post.status_code
+                elif ops_post.status_code == 422:
+                    print('Request validation error. Check "errors" array for details.')
+                    ops_post.close()
+                    logger.error(f'API Call: {ops_post.status_code} Request validation error. Check "errors" array for details.')
+                    return ops_post.status_code
+                elif ops_post.status_code == 500:
+                    print("Unexpected server side error.")
+                    ops_data = loads(ops_post.text)
+                    ops_post.close()
+                    logger.error(f'API Call: {ops_post.status_code} Unexpected server side error.')
+                    return ops_post.status_code
+                else:
+                    print("POST Request Failed")
+                    ops_post.close()
+                    logger.error(f'API Call: {ops_post.status_code} Request Failed...Unknown why')
+                    return ops_post.status_code
+            except RequestException as exc:
+                print(f"An error occurred while requesting {exc.request.url!r}.")
+        
+        else:
+            try:
+                ops_post = self.session.post(url, params=encoded_params, json=self.payload)
+
+                if ops_post.status_code == 201:
+                    print("POST Request Successful!")
+                    ops_data = ops_post.text
+                    logger.info(f'API Call: {ops_post.status_code} POST Request Successful! {ops_data}')
+                    ops_post.close()
+                    return ops_post.status_code       
+                elif ops_post.status_code == 400:
+                    print("JSON error. Check the JSON format.")
+                    ops_post.close()
+                    logger.error(f'API Call: {ops_post.status_code} JSON error. Check the JSON format.')
+                    return ops_post.status_code
+                elif ops_post.status_code == 401:
+                    print("Token error. Login again.")
+                    ops_post.close()
+                    logger.error(f'API Call: {ops_post.status_code} Authentication error.')
+                    return ops_post.status_code
+                elif ops_post.status_code == 403:
+                    print("Insufficient permissions to access this resource.")
+                    ops_post.close()
+                    logger.error(f'API Call: {ops_post.status_code} Insufficient permissions to access this resource.')
+                    return ops_post.status_code
+                elif ops_post.status_code == 409:
+                    print("The submitted resource conflicts with another.")
+                    ops_post.close()
+                    logger.error(f'API Call: {ops_post.status_code} The submitted resource conflicts with another.')
+                    return ops_post.status_code
+                elif ops_post.status_code == 422:
+                    print('Request validation error. Check "errors" array for details.')
+                    ops_post.close()
+                    logger.error(f'API Call: {ops_post.status_code} Request validation error. Check "errors" array for details.')
+                    return ops_post.status_code
+                elif ops_post.status_code == 500:
+                    print("Unexpected server side error.")
+                    ops_data = loads(ops_post.text)
+                    ops_post.close()
+                    logger.error(f'API Call: {ops_post.status_code} Unexpected server side error.')
+                    return ops_post.status_code
+                else:
+                    print("POST Request Failed")
+                    ops_post.close()
+                    logger.error(f'API Call: {ops_post.status_code} Request Failed...Unknown why')
+                    return ops_post.status_code
+            except RequestException as exc:
+                print(f"An error occurred while requesting {exc.request.url!r}.")
+
+    def put_operations(self, ops_type: str, url_var: str, payload: dict, params = {}, json=True) -> Response:
+        """ API PUT operations """
+
+        ### VARS ###
+        self.ops_type = ops_type
+        self.url_var = url_var
+        self.payload = payload
+        self.params = params
+        encoded_params = urlencode(self.params)
+        self.json = json
+        url = f"{self.url_var}/{self.ops_type}"
+
+        if self.json:
+            try:
+                ops_put = self.session.put(url, params=encoded_params, json=self.payload)
+                
+                if ops_put.status_code == 200:
+                    print("PUT Request Successful!")
+                    ops_data = loads(ops_put.text)
+                    ops_put.close()
+                    logger.info(f'API Call: {ops_put.status_code} PUT Request Successful! {ops_data}')
+                    return ops_put.status_code      
+                elif ops_put.status_code == 400:
+                    print("JSON error. Check the JSON format.")
+                    ops_put.close()
+                    logger.error(f'API Call: {ops_put.status_code} JSON error. Check the JSON format.')
+                    return ops_put.status_code
+                elif ops_put.status_code == 401:
+                    print("Token error. Login again.")
+                    ops_put.close()
+                    logger.error(f'API Call: {ops_put.status_code} Authentication error.')
+                    return ops_put.status_code
+                elif ops_put.status_code == 403:
+                    print("Insufficient permissions to access this resource.")
+                    ops_put.close()
+                    logger.error(f'API Call: {ops_put.status_code} Insufficient permissions to access this resource.')
+                    return ops_put.status_code
+                elif ops_put.status_code == 409:
+                    print("The submitted resource conflicts with another.")
+                    ops_put.close()
+                    logger.error(f'API Call: {ops_put.status_code} The submitted resource conflicts with another.')
+                    return ops_put.status_code
+                elif ops_put.status_code == 422:
+                    print('Request validation error. Check "errors" array for details.')
+                    ops_put.close()
+                    logger.error(f'API Call: {ops_put.status_code} Request validation error. Check "errors" array for details.')
+                    return ops_put.status_code
+                elif ops_put.status_code == 500:
+                    print("Unexpected server side error.")
+                    ops_data = loads(ops_put.text)
+                    ops_put.close()
+                    logger.error(f'API Call: {ops_put.status_code} Unexpected server side error.')
+                    return ops_data, ops_put.status_code
+                else:
+                    print("POST Request Failed")
+                    ops_put.close()
+                    logger.error(f'API Call: {ops_put.status_code} Request Failed...Unknown why')
+                    return ops_put.status_code
+            except RequestException as exc:
+                print(f"An error occurred while requesting {exc.request.url!r}.")
+
+        else:
+            try:
+                ops_put = self.session.put(url,params=encoded_params, json=payload)
+                
+                if ops_put.status_code == 201:
+                    print("PUT Request Successful!")
+                    ops_data = ops_put.text
+                    logger.info(f'API Call: {ops_put.status_code} POST Request Successful! {ops_data}')
+                    ops_put.close()
+                    return ops_put.status_code     
+                elif ops_put.status_code == 400:
+                    print("JSON error. Check the JSON format.")
+                    ops_put.close()
+                    logger.error(f'API Call: {ops_put.status_code} JSON error. Check the JSON format.')
+                    return ops_put.status_code
+                elif ops_put.status_code == 401:
+                    print("Token error. Login again.")
+                    ops_put.close()
+                    logger.error(f'API Call: {ops_put.status_code} Authentication error.')
+                    return ops_put.status_code
+                elif ops_put.status_code == 403:
+                    print("Insufficient permissions to access this resource.")
+                    ops_put.close()
+                    logger.error(f'API Call: {ops_put.status_code} Insufficient permissions to access this resource.')
+                    return ops_put.status_code
+                elif ops_put.status_code == 409:
+                    print("The submitted resource conflicts with another.")
+                    ops_put.close()
+                    logger.error(f'API Call: {ops_put.status_code} The submitted resource conflicts with another.')
+                    return ops_put.status_code
+                elif ops_put.status_code == 422:
+                    print('Request validation error. Check "errors" array for details.')
+                    ops_put.close()
+                    logger.error(f'API Call: {ops_put.status_code} Request validation error. Check "errors" array for details.')
+                    return ops_put.status_code
+                elif ops_put.status_code == 500:
+                    print("Unexpected server side error.")
+                    ops_data = loads(ops_put.text)
+                    ops_put.close()
+                    logger.error(f'API Call: {ops_put.status_code} Unexpected server side error.')
+                    return ops_data, ops_put.status_code
+                else:
+                    print("POST Request Failed")
+                    ops_put.close()
+                    logger.error(f'API Call: {ops_put.status_code} Request Failed...Unknown why')
+                    return ops_put.status_code
+            except RequestException as exc:
+                print(f"An error occurred while requesting {exc.request.url!r}.")
---
+++ network_automation/guest_network/guest_nw.py
@@ -0,0 +1,105 @@
+#! /usr/bin/env python
+"""
+Script to add MAC addres to correct identity group
+"""
+from decouple import config
+import logging
+from pathlib import Path
+import urllib3
+import sys
+import os
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+from netaddr import EUI
+from IseApi import Ise
+
+def is_valid_mac_address(mac_address: str) -> str:
+    """
+    Check if a given string is a valid MAC address.
+
+    Args:
+        mac_address (str): The string to be checked as a MAC address.
+
+    Returns:
+        EUI or False: If the input is a valid MAC address, returns the EUI (Extended Unique Identifier) object representing the MAC address. 
+        If the input is not a valid MAC address, returns False.
+    """
+
+    try:
+        mac = EUI(mac_address)
+        return mac
+    except:
+        return False
+
+def main():
+    """
+    Main function to retrieve and process MAC address information from ISE.
+
+    This function establishes an ISE session, retrieves active sessions, filters
+    guest MAC addresses, retrieves MAC address information, and updates the 
+    endpoint group for unknown MAC addresses.
+    """
+    
+    ### LOGGING SETUP ###
+    LOG_FILE = Path("logs/ise_ops.log")
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler(LOG_FILE)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)  
+
+    ### DISABLE WARNINGS ###
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    ### VARS ###
+    ISE_URL = config("ISE_URL_VAR")
+    ISE_URL_MONITORING = ISE_URL.replace(":9060","")
+    ISE_AUTHENTICATION = config("ISE_AUTHENTICATION")
+    ISE_GUEST_ENDPOINT_GROUP_ID = config("ISE_GUEST_ENDPOINT_GROUP_ID")
+    mac_data = []
+    
+
+    ### INITIALIZE API OBJECT AND GET THE ACTIVE SESSIONS LIST ###
+    ise_xml = Ise(ISE_AUTHENTICATION, "xml")
+    logger.info(f'ISE Session Established.')
+    active_list = ise_xml.get_operations("admin/API/mnt/Session/ActiveList", ISE_URL_MONITORING)
+    logger.info(f'Active Sessions Retrieved.')
+
+    ### FILTER GUEST MACS ### 
+    guest_endpoint_list = [session["user_name"] for session in active_list["activeList"]["activeSession"] if "framed_ip_address" in session and "192.168" in session["framed_ip_address"] and is_valid_mac_address(session["user_name"])]
+    logger.info(f'Active sessions filtered by guest networks')
+
+    ### GET MAC ADDRESS INFORMATION FROM FILTERED MACS ###
+    for mac in guest_endpoint_list:
+        result = ise_xml.get_operations(f'admin/API/mnt/Session/MACAddress/{mac}', ISE_URL_MONITORING)
+        mac_data.append(result["sessionParameters"])
+    logger.info(f'Retrieved MAC address information for filtered active sessions.')
+
+    macs_unknown = [mac["user_name"] for mac in mac_data if "endpoint_policy" in mac and "user_name" in mac and mac["endpoint_policy"] == "Unknown"]
+    logger.info(f'MAC addresses filtered that belong to the "Unknown" Endpoint Group')
+
+    ### INITIALIZE API OBJECT AND GET THE ACTIVE SESSIONS LIST ###
+    ise_json = Ise(ISE_AUTHENTICATION)
+    logger.info(f'New ISE Session Established.')
+
+    ### ADD MAC ADDRESSES TO GUEST ENDPOINT GROUP ###
+    for mac in macs_unknown:
+        user_id = ise_json.get_operations(f'ers/config/endpoint/name/{mac}', ISE_URL)
+        logger.info(f'Retrieved endpoint id for {mac}')
+        payload = {"ERSEndPoint": {"name": mac, "mac": mac, "staticGroupAssignment": "true", "groupId": ISE_GUEST_ENDPOINT_GROUP_ID}}
+        result = ise_json.put_operations(f'ers/config/endpoint/{user_id["ERSEndPoint"]["id"]}', ISE_URL, payload)
+        if result == 200:
+            logger.info(f'Updated FLS-Guest-Endpoint-PoC with {mac}')
+        else:
+            logger.error(f'Updating FLS-Guest-Endpoint-PoC with {mac} FAILED!')            
+
+if __name__ == '__main__' :
+    """
+    The main execution block of the script.
+
+    This block is executed only if the script is run directly (not imported as a module).
+    It calls the main() function to start the process of retrieving and processing MAC address information from ISE.
+    """
+    main()
