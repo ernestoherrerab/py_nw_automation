838d06e5c9f2f0603b2271767635e42c337e8443
Ernesto <44853461+ernestoherrerab@users.noreply.github.com>
2023-10-09 09:19:24 +0200

    migrate prisma onboarding from frontend to cli

---
+++ network_automation/prisma_onboarding/infoblox_ops.py
@@ -0,0 +1,95 @@
+#! /usr/bin/env python
+"""
+Script to Provision IPSec Tunnel IPs
+"""
+from decouple import config
+import logging
+from pathlib import Path
+import re
+import os
+import sys
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+from netaddr import IPNetwork
+from libs.InfobloxApi import Infoblox
+
+### LOGGING SETUP ###
+LOG_FILE = Path("logs/sdwan_ops.log")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(LOG_FILE)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+def create_tunnel_ips(num_nws: list, site_data: dict) -> list:
+    """Provision SDWAN Tunnels IPs
+
+    Args:
+    num_nws (list): Number of required networks
+    site_data (dict): Frontend input data
+
+    Returns 
+    tunnel_ips_list (list): List of IP addresses to use for the SDWAN tunnels
+    """
+    ### VARS ###
+    INFOBLOX_URL = config("INFOBLOX_URL")
+    INFOBLOX_AUTHENTICATION = config("INFOBLOX_AUTHENTICATION")
+    TUNNEL_NETWORK = config("INFOBLOX_TUNNEL_NETWORK")
+    site_code = site_data["site_code"].lower()
+    payload = {
+                  "network": {
+                    "_object_function": "next_available_network",
+                    "_parameters": {
+                      "cidr": 30
+                    },
+                    "_result_field": "networks",
+                    "_object": "networkcontainer",
+                    "_object_parameters": {
+                      "network": TUNNEL_NETWORK
+                    }
+                  }
+                }
+    iterations = len(num_nws)
+    print(iterations)
+    
+    
+    ### POST API CALL TO CREATE NEXT AVAILABLE /30 NETWORK ###
+    print("Creating networks in Infoblox...")
+    response_list = []
+    counter = 1
+    infoblox_network = Infoblox(INFOBLOX_AUTHENTICATION)
+  
+    while counter <= iterations:
+      response, _ = infoblox_network.post_operations("network", INFOBLOX_URL, payload=payload)
+      print(response)
+      response_list.append(response)
+      logger.info(f'Infoblox: Subnet Created {response}')
+      counter += counter
+
+    ### ADD SITE ID TO COMMENTS ###
+    print("Documenting Site Code in Infoblox...")
+    print(response_list)
+    for network in response_list:
+      payload = {"comment": site_code.upper()}
+      response = infoblox_network.put_operations(network, INFOBLOX_URL, payload=payload)
+      logger.info(f'Infoblox: Subnet Edited {response}')
+    
+    ### GET SUBNETS FOR TUNNELS ###
+    print("Getting Tunnel IP...")
+    tunnel_subnet_list = []
+    for subnet in response_list:
+      tunnel_subnet = re.findall(r'network\/\w+:(\S+)\/default', subnet)
+      tunnel_subnet_list.append(tunnel_subnet[0])
+      logger.info(f'Infoblox: Tunnel subnets {tunnel_subnet[0]}')
+
+    ### GET TUNNEL IPS FOR TUNNELS ###
+    tunnel_ips_list = []
+    for tunnel_ip in tunnel_subnet_list:
+      network = IPNetwork(tunnel_ip)
+      tunnel_ip_list = list(network)
+      tunnel_ips_list.append(str(tunnel_ip_list[1]))
+      logger.info(f'Infoblox: Tunnel IP {tunnel_ip_list[1]}')
+      
+    return tunnel_ips_list
---
+++ network_automation/prisma_onboarding/provision_tunnel.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+"""
+Script to Provision Prisma Access Tunnels
+"""
+from getpass import getpass
+import logging
+from pathlib import Path
+import urllib3
+import infoblox_ops
+import ipfabric_ops
+import prisma_access_ops
+import sdwan_ops
+
+### LOGGING SETUP ###
+LOG_FILE = Path("logs/sdwan_ops.log")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(LOG_FILE)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+def main():
+    """ Main function to provision tunnels """
+    
+    ### DISABLE CERTIFICATE WARNINGS ###
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    ### VARS ###
+    site_data = {}
+
+
+    print(
+        '''Welcome to the Prisma Access Onboarding Tool\n\n
+        * The compute location and Prisma Access location can be found in:
+          Prisma Access -> Service Setup -> Remote Networks -> Bandwidth Management\n
+        ** The locations must be exactly entered as displayed
+        *** BANDWIDTH MUST HAVE BEEN ALLOCATED TO THE REGION ALREADY\n
+        '''
+    )
+    username = input("vManage Username: ")
+    password = getpass("vManage Password: ")
+    site_code = input("Site code: ")
+    region_id = input("Compute Location: ")
+    location_id = input("Prisma Access Location:")
+    site_data["site_code"] = site_code.lower()
+    site_data["region_id"] = region_id
+    site_data["location_id"] = location_id
+
+    ### GET SDWAN IPSEC FEATURE TEMPLATE VALUES ###
+    ### GET SUBNETS FOR REMOTE NETWORKS IN PRISMA ACCESS ###
+    hostname_ip_set, remote_nw_subnets = ipfabric_ops.get_ipfabric_data(site_data)
+    print(hostname_ip_set, remote_nw_subnets)
+#    
+#    ### PROVISION TUNNEL INTERFACES IN INFOBLOX ###
+#    infoblox_response = infoblox_ops.create_tunnel_ips(hostname_ip_set, site_data)
+#    
+#    ### CREATE REMOTE NETWORKS IN PRISMA ACCESS ###
+#    ### GET PUBLIC IP FOR SDWAN TUNNEL DESTINATION ###
+#    public_ip, bgp_asn, bgp_peers = prisma_access_ops.create_remote_networks(site_data, hostname_ip_set, remote_nw_subnets, infoblox_response)
+#
+#    ### CREATE IPSEC TUNNELS ON SDWAN VMANAGE ###
+#    summary_list = sdwan_ops.create_ipsec_tunnels(site_data, username, password, hostname_ip_set, public_ip, bgp_asn, bgp_peers, infoblox_response)
+#
+    return hostname_ip_set
+    #return summary_list
+
+if __name__ == '__main__' :
+    """
+    The main execution block of the script.
+
+    This block is executed only if the script is run directly (not imported as a module).
+    It calls the main() function to start the process of retrieving and processing MAC address information from ISE.
+    """
+    main()
